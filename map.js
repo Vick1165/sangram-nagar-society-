0d6ad8b92e80d6978f6d682d869d3d2c2870d1f2
Vick1165 <vickykumar538@gmail.com>
2020-09-27 18:07:39 +0530

    added maehs store in map location

--- map.js
+++ map.js
@@ -1,10 +1,11 @@
 // Initialize and add the map
 function initMap() {
   // The location of Uluru
-  var uluru = { lat: 19.049511, lng: 72.861129 };
+  //   var uluru = { lat: 19.049511, lng: 72.861129 };
+  var uluru = { lat: 19.0495569, lng: 72.8609117 };
   // The map, centered at Uluru
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 30,
+    zoom: 20,
     center: uluru,
   });
   // The marker, positioned at Uluru
